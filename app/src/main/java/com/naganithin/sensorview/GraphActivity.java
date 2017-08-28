c2e9410da0b2eb629cfe452d74fe33821d45c14f
Naganithin Manne <nithinmanne@gmail.com>
2017-08-28 16:59:28 +0530

    Completed Code

--- app/src/main/java/com/naganithin/sensorview/GraphActivity.java
+++ app/src/main/java/com/naganithin/sensorview/GraphActivity.java
@@ -11,23 +11,29 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Locale;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class GraphActivity extends AppCompatActivity implements SensorEventListener {
+
 
     int [] sensorList;
     String [] sensorNames;
     int [] sensors;
-    TextView [] sensorViews;
+    LineGraphSeries<DataPoint>[] sensorViews;
+    private final int maxData = 200;
+    int [] lastx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_graph);
         sensorList = new int[]{-1, -1, -1, -1};
+        lastx = new int[]{0, 0, 0, 0};
         sensors = new int[]{Sensor.TYPE_LIGHT, Sensor.TYPE_PRESSURE, Sensor.TYPE_AMBIENT_TEMPERATURE, Sensor.TYPE_RELATIVE_HUMIDITY, Sensor.TYPE_ACCELEROMETER, Sensor.TYPE_GYROSCOPE, Sensor.TYPE_MAGNETIC_FIELD, Sensor.TYPE_PROXIMITY};
         sensorNames = new String[]{"Light", "Pressure", "Temperature", "Humidity", "Accelerometer", "Gyroscope", "Magnetometer", "Proximity"};
-        sensorViews = new TextView[4];
+        sensorViews = new LineGraphSeries[4];
     }
 
     @Override
@@ -46,46 +52,72 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             if(count==4) break;
         }
 
-        TextView l1, l2, l3, l4, v1, v2, v3, v4;
-        l1 = findViewById(R.id.l1);
-        l2 = findViewById(R.id.l2);
-        l3 = findViewById(R.id.l3);
-        l4 = findViewById(R.id.l4);
-        v1 = findViewById(R.id.v1);
-        v2 = findViewById(R.id.v2);
-        v3 = findViewById(R.id.v3);
-        v4 = findViewById(R.id.v4);
-        v1.setText("");
-        v2.setText("");
-        v3.setText("");
-        v4.setText("");
+        TextView l1, l2, l3, l4;
+        l1 = findViewById(R.id.lv1);
+        l2 = findViewById(R.id.lv2);
+        l3 = findViewById(R.id.lv3);
+        l4 = findViewById(R.id.lv4);
+        LineGraphSeries<DataPoint> series, series2, series3, series4;
+        series = new LineGraphSeries<>();
+        series2 = new LineGraphSeries<>();
+        series3 = new LineGraphSeries<>();
+        series4 = new LineGraphSeries<>();
+        series.appendData(new DataPoint(0, 0), true, maxData);
+        series2.appendData(new DataPoint(0, 0), true, maxData);
+        series3.appendData(new DataPoint(0, 0), true, maxData);
+        series4.appendData(new DataPoint(0, 0), true, maxData);
+        GraphView g1, g2, g3, g4;
+        g1 = findViewById(R.id.g1);
+        g2 = findViewById(R.id.g2);
+        g3 = findViewById(R.id.g3);
+        g4 = findViewById(R.id.g4);
+        g1.addSeries(series);
+        g2.addSeries(series2);
+        g3.addSeries(series3);
+        g4.addSeries(series4);
+        g1.getViewport().setXAxisBoundsManual(true);
+        g1.getViewport().setMinX(0);
+        g1.getViewport().setMaxX(maxData);
+        g2.getViewport().setXAxisBoundsManual(true);
+        g2.getViewport().setMinX(0);
+        g2.getViewport().setMaxX(maxData);
+        g3.getViewport().setXAxisBoundsManual(true);
+        g3.getViewport().setMinX(0);
+        g3.getViewport().setMaxX(maxData);
+        g4.getViewport().setXAxisBoundsManual(true);
+        g4.getViewport().setMinX(0);
+        g4.getViewport().setMaxX(maxData);
         if(sensorList[0]==-1) {
             l1.setText("");
+            g1.setVisibility(View.INVISIBLE);
         }
         else {
             l1.setText(sensorNames[sensorList[0]]);
-            sensorViews[0] = v1;
+            sensorViews[0] = series;
         }
         if(sensorList[1]==-1) {
             l2.setText("");
+            g2.setVisibility(View.INVISIBLE);
         }
         else {
             l2.setText(sensorNames[sensorList[1]]);
-            sensorViews[1] = v2;
+            sensorViews[1] = series2;
         }
         if(sensorList[2]==-1) {
             l3.setText("");
+            g3.setVisibility(View.INVISIBLE);
         }
         else {
             l3.setText(sensorNames[sensorList[2]]);
-            sensorViews[2] = v3;
+            sensorViews[2] = series3;
         }
         if(sensorList[3]==-1) {
             l4.setText("");
+            g4.setVisibility(View.INVISIBLE);
         }
         else {
             l4.setText(sensorNames[sensorList[3]]);
-            sensorViews[3] = v4;
+            sensorViews[3] = series4;
         }
     }
 
@@ -117,24 +149,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             i++;
         }
         if(i==4) return;
-        TextView v = sensorViews[i];
+        LineGraphSeries<DataPoint> v = sensorViews[i];
         float x = sensorEvent.values[0];
         float y = sensorEvent.values[1];
         float z = sensorEvent.values[2];
         double mag = Math.sqrt(x*x+y*y+z*z);
-        //v.setText(String.format(Locale.getDefault(), "%.3f %.3f %.3f", x, y, z));
-        v.setText(String.format(Locale.getDefault(), "%.9f", mag));
+        lastx[i]++;
+        v.appendData(new DataPoint(lastx[i], mag), true, maxData);
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 
-    public void graph(View v) {
-        Intent k = new Intent(MainActivity.this, GraphActivity.class);
+    public void back(View v) {
+        Intent k = new Intent(GraphActivity.this, MainActivity.class);
         startActivity(k);
         finish();
     }
-
 
 }
